e6f32b01cc9280cc29befbda67067fbbbb6778e4
Mo7ammedd <medo.mostafa22255@icloud.com>
2024-02-17 16:27:20 +0200

    add c# sol

---
+++ c#/1. Two Sum.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (numDict.ContainsKey(complement))
+            {
+                return new int[] { numDict[complement], i };
+            }
+
+            numDict[nums[i]] = i;
+        }
+
+        return null;
+    }
+}

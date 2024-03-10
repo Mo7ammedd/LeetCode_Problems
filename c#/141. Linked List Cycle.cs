1da525e7b9fb60ca9974f898e29f76117a61a5fc
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-03-10 16:33:31 +0200

    add c# sol

---
+++ c#/141. Linked List Cycle.cs
@@ -0,0 +1,12 @@
+  public bool HasCycle(ListNode head) {
+        Dictionary<ListNode, int> dt = new Dictionary<ListNode, int>();
+        while (head != null){
+            if (dt.ContainsKey(head)){
+                return true;
+            }
+            else 
+            dt[head] = 1;  
+            head = head.next;
+        }
+        return false;
+    }

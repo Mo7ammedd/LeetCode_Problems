6b47e2f75a37241411ff4b2a3d72c3039ce20691
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-03-12 16:12:28 +0200

    add c# sol

---
+++ c#/19. Remove Nth Node From End of List.cs
@@ -0,0 +1,25 @@
+  public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        ListNode currentNode = head;
+        List<ListNode> list = new List<ListNode>();
+
+        while (currentNode != null)
+        {
+            list.Add(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        if (list.Count == 1)
+        {
+            return null;
+        }
+        else if (list.Count == n)
+        {
+            return head.next;
+        }
+
+        list[list.Count - n - 1].next = list[list.Count - n].next;
+
+        return head;
+    }
+}

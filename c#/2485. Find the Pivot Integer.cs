8d2c82ee61e04f27a955c80c2df31f896c0f73ad
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-03-13 16:47:14 +0200

    add c# sol

---
+++ c#/2485. Find the Pivot Integer.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int PivotInteger(int n) {
+        double pivot = Math.Sqrt((n * (n + 1)) / 2);
+        if(pivot - Math.Floor(pivot) == 0)
+            return (int)pivot;
+        return -1;
+    }
+}
+
+
+
+
+zzzzzzz

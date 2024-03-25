d39ae167fca1e3cc01161b582b7a1f2f5a43b2a8
Mohammed Mostafa <128194288+Mo7ammedd@users.noreply.github.com>
2024-03-25 02:26:33 +0200

    [27] [Time Beats: 10.08%] [Memory Beats: 18.29%] - LeetPush

--- 27-Remove-Element.cs
+++ 27-Remove-Element.cs
@@ -1,16 +1,19 @@
 public class Solution {
-    public int RemoveElement(int[] nums, int val) {
-
-        int k = 0;
-        for(int i = 0; i < nums.Length; i++)
-        {
-            if(nums[i] != val)
+    public int RemoveElement(int[] nums, int val) 
+    { 
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
             {
-                nums[k] = nums[i];
-                k++;
+                if (nums[i] != val)
+                {
+                   nums[count++]=nums[i];
+                  
+                }
+              
+
             }
-        }
-        
-        return k;
+
+            return count;
+
     }
 }
